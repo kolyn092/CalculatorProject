e8ac8f8aea82a9a12f9e0daa3a26249a80d2c84d
kolyn092 <kolyn092@gmail.com>
2026-03-19 12:17:21 +0900

    fix: do-while을 while로 변경

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -14,14 +14,13 @@ public class Calculator {
 
         // exit 문자열을 입력하기 전까지 무한으로 진행함
         while (true) {
-            // do-while 조건 처리를 하기 위해 num1과 num2의 초기값을 -1로 설정
-            int num1 = -1;          // 첫 번째 숫자
-            int num2 = -1;          // 두 번째 숫자
+            int num1;               // 첫 번째 숫자
+            int num2;               // 두 번째 숫자
             char operator;          // 사칙 연산 기호
             int result = 0;         // 연산 결과 값
 
             // 양의 정수 (0 포함) 입력 받기
-            do {
+            while (true) {
                 // 정수가 아닌 경우 try-catch로 NumberFormatException 처리
                 try {
                     System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -29,23 +28,29 @@ public class Calculator {
                     System.out.print("두 번째 숫자를 입력하세요: ");
                     num2 = Integer.parseInt(scanner.nextLine());
 
+                    if ((num1 < 0 || num2 < 0)) {
+                        System.out.println("양의 숫자(0 포함)를 입력해주세요.");
+                        continue;
+                    }
+                    break;
+
                 } catch (NumberFormatException ex) {
                     System.out.println("양의 숫자(0 포함)를 입력해주세요. " + ex);
                 }
-                // 0보다 작은 경우 입력을 다시 받음
-            } while (num1 < 0 || num2 < 0);
+            }
 
             // 사칙 연산 기호 (+, -, *, /) 입력 받기
-            do {
+            while (true) {
                 System.out.print("사칙 연산 기호를 입력하세요: ");
                 operator = scanner.nextLine().charAt(0);
 
                 // isNotOperator 함수를 사용하여 사칙 연산 기호인지 판별
                 if (isNotOperator(operator)) {
                     System.out.println("올바른 사칙 연산 기호를 입력하세요. (+, -, *, /)");
+                    continue;
                 }
-                // 사칙 연산 기호가 아니라면 재입력
-            } while (isNotOperator(operator));
+                break;
+            }
 
             // 입력 받은 정수 2개와 사칙 연산 기호를 사용하여 연산 진행 후 출력
             try {
